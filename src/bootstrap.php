46fec8a6e24154f7267e5b2141e2cd0495b87e3f
miracletanie <michelledian23@gmail.com>
2023-02-13 13:36:40 +0000

    first commit

---
+++ src/bootstrap.php
@@ -0,0 +1,11 @@
+
+<?php
+
+session_start();
+
+
+require_once __DIR__ . '/libs/helpers.php';
+require_once __DIR__ . '/libs/flash.php';
+require_once __DIR__ . '/libs/sanitization.php';
+require_once __DIR__ . '/libs/validation.php';
+require_once __DIR__ . '/libs/filter.php';
